fceaaa0a7f649f1991e7dba5d6a73fac80b7180d
Jonathan Balls <j.balls@btopenworld.com>
2016-05-06 00:40:02 +0100

    It's working. Nobody touch anything...

--- cabot_check_icmp/plugin.py
+++ cabot_check_icmp/plugin.py
@@ -5,21 +5,47 @@ from django import forms
 from django.template import Context, Template
 
 from cabot.plugins.models import CheckPlugin
+from cabot.plugins.forms import CheckConfigForm
+from cabot.cabotapp.models import StatusCheckResult
 
 from os import environ as env
+import subprocess
 import requests
 import logging
+
+
+class ICMPStatusCheckForm(CheckConfigForm):
+    name = forms.CharField(max_length=80)
+
 
 class ICMPCheckPlugin(CheckPlugin):
     name = "ICMP"
     slug = "icmp"
-    author = "Jonatham Balls"
+    author = "Jonathan Balls"
     version = "0.0.1"
+    font_icon = "glyphicon glyphicon-transfer"
+
+    config_form = ICMPStatusCheckForm
+
+    def target(self, check):
+        instances = check.instance_set.all()
+        services = check.service_set.all()
+        if instances:
+            return instances[0]
+        elif services:
+            return services[0]
+        else:
+            return None
     
-    def _run(self):
-        result = StatusCheckResult(status_check=self)
-        instances = self.instance_set.all()
-        target = self.instance_set.get().address
+    def run(self, check):
+        result = StatusCheckResult(status_check=check)
+        instances = check.instance_set.all()
+        services = check.service_set.all()
+        
+        if instances:
+            target = instances[0].address
+        else:
+            target = services[0].url
 
         # We need to read both STDOUT and STDERR because ping can write to both, depending on the kind of error. Thanks a lot, ping.
         ping_process = subprocess.Popen("ping -c 1 " + target, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
@@ -34,6 +60,10 @@ class ICMPCheckPlugin(CheckPlugin):
 
         return result
 
-class ICMPStatusCheckForm(forms.Form):
-    name = forms.CharField(max_length=80)
+    def description(self, check):
+        target = self.target(check)
+        if target:
+            return 'ICMP Reply from {}'.format(target.name)
+        else:
+            return 'ICMP Check with no target.'
 
